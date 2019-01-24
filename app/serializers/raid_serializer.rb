43c5d158f429daa925231fbb3918ecde076609b2
cedis81 <ccchom11@gmail.com>
2019-01-24 15:10:56 -0500

    Change user_id to user to retrieve all user data

--- app/serializers/raid_serializer.rb
+++ app/serializers/raid_serializer.rb
@@ -1,5 +1,5 @@
 class RaidSerializer < ActiveModel::Serializer
-  attributes :id, :boss_name, :time_remaining, :user_id, :editable
+  attributes :id, :boss_name, :time_remaining, :user, :editable
 
   def editable
     scope == object.user
